6cb2faeb66ec726733dc03060e86d643586542c0
Cameron <weijmer.c@gmail.com>
2022-05-22 18:11:46 +0200

    Javascript for only one joke

--- fetch.js
+++ fetch.js
@@ -1,21 +1,18 @@
+let jok = document.getElementById('hello');
+
 const options = {
-    method: 'GET',
-    headers: {
-        accept: 'application/json',
-        'X-RapidAPI-Host': 'matchilling-chuck-norris-jokes-v1.p.rapidapi.com',
-        'X-RapidAPI-Key': '345cd23516mshed85a5e5a8a352fp1b3b3ajsn468a1f67f88a'
-    }
+	method: 'GET',
+	headers: {
+		accept: 'application/json',
+		'X-RapidAPI-Host': 'matchilling-chuck-norris-jokes-v1.p.rapidapi.com',
+		'X-RapidAPI-Key': '345cd23516mshed85a5e5a8a352fp1b3b3ajsn468a1f67f88a'
+	}
 };
 
-fetch('https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/search?query=video%20game', options)
+fetch('https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random', options)
 .then(response => {
-    return response.json()
+    return response.json();
 }).then(jokes => {
-    // console.log(jokes.result);
-    const comedy = jokes.result.map(jokeline => {
-        return `
-            <p class="joke">${jokeline.value}</p>
-        `
-    })
-    document.querySelector("#cnjokes").insertAdjacentHTML('afterbegin', comedy);
+    console.log(jokes.value);
+    jok.innerHTML = jokes.value;
 })
