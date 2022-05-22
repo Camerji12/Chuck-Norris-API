c113e7f4c55d1b279d29691b6350e8299469c535
Cameron <weijmer.c@gmail.com>
2022-05-22 19:32:48 +0200

    New Joke function + background

---
+++ src/fetch.js
@@ -0,0 +1,39 @@
+let jok = document.getElementById('hello');
+
+const options = {
+	method: 'GET',
+	headers: {
+		accept: 'application/json',
+		'X-RapidAPI-Host': 'matchilling-chuck-norris-jokes-v1.p.rapidapi.com',
+		'X-RapidAPI-Key': '345cd23516mshed85a5e5a8a352fp1b3b3ajsn468a1f67f88a'
+	}
+};
+
+fetch('https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random', options)
+	.then(response => {
+		return response.json();
+	}).then(jokes => {
+		console.log(jokes.value);
+		jok.innerHTML = jokes.value;
+	})
+
+function newjoke() {
+	let jok = document.getElementById('hello');
+
+	const options = {
+		method: 'GET',
+		headers: {
+			accept: 'application/json',
+			'X-RapidAPI-Host': 'matchilling-chuck-norris-jokes-v1.p.rapidapi.com',
+			'X-RapidAPI-Key': '345cd23516mshed85a5e5a8a352fp1b3b3ajsn468a1f67f88a'
+		}
+	};
+
+	fetch('https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random', options)
+		.then(response => {
+			return response.json();
+		}).then(jokes => {
+			console.log(jokes.value);
+			jok.innerHTML = jokes.value;
+		})
+}
